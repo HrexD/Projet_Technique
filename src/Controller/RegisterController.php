d90164275d5fab7e2cbe09f003851883c3ed06da
Hrex <cedgoujon78@gmail.com>
2023-06-29 13:59:55 +0200

    Save picture

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,4 +1,5 @@
 <?php
+
 // src/Controller/RegisterController.php
 
 namespace App\Controller;
@@ -7,55 +8,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
+use App\Entity\User;
 
 class RegisterController extends AbstractController
 {
-#[Route('/register', name: 'app_register')]
-public function register(Request $request, SluggerInterface $slugger): Response
-{
-if ($request->isMethod('POST')) {
-$data = $request->request->all();
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request): Response
+    {
+        if ($request->isMethod('POST')) {
+            $nom = $request->request->get('nom');
+            $prenom = $request->request->get('prenom');
 
-// Récupérer les données du formulaire
-$nom = $data['nom'];
-$prenom = $data['prenom'];
-$role = $data['role'];
+            // Enregistrer la photo capturée dans le dossier public/images
+            $photoFileName = $nom . '_' . time() . '.jpg';
+            $photoPath = $this->getParameter('images_directory') . '/' . $photoFileName;
+            file_put_contents($photoPath, file_get_contents('php://input'));
 
-// Récupérer l'image de la webcam
-$imageData = $data['image'];
-$imageData = str_replace('data:image/jpeg;base64,', '', $imageData);
-$imageData = base64_decode($imageData);
+            // Enregistrer les autres données de l'utilisateur dans la base de données
+            $entityManager = $this->getDoctrine()->getManager();
 
-// Générer un SluggerId unique pour le nom de l'image
-$sluggerId = $slugger->slug(uniqid());
+            // Créer une instance de l'entité User et définir les valeurs
+            $user = new User();
+            $user->setNom($nom);
+            $user->setPrenom($prenom);
+            $user->setPhoto($photoFileName);
 
-// Définir le nom de l'image avec le SluggerId
-$imageName = 'user_photo_' . $sluggerId . '.jpg';
+            // Persist et flush l'entité dans la base de données
+            $entityManager->persist($user);
+            $entityManager->flush();
 
-// Chemin de destination pour enregistrer l'image
-$imagePath = $this->getParameter('kernel.project_dir') . '/public/images/' . $imageName;
+            // Rediriger ou retourner une réponse appropriée
+            return $this->redirectToRoute('app_register_success');
+        }
 
-file_put_contents($imagePath, $imageData);
+        return $this->render('register/index.html.twig');
+    }
 
-// Enregistrer les données dans la table User
-// Remplacez le code suivant par votre propre logique d'enregistrement
-
-// Exemple :
-$user = new User();
-$user->setNom($nom);
-$user->setPrenom($prenom);
-$user->setRole($role);
-$user->setPhoto($imageName);
-
-$entityManager = $this->getDoctrine()->getManager();
-$entityManager->persist($user);
-$entityManager->flush();
-
-// Rediriger vers une page de succès ou afficher un message de succès
-return $this->redirectToRoute('app_register_success');
-}
-
-return $this->render('register/index.html.twig');
-}
+    #[Route('/register/success', name: 'app_register_success')]
+    public function registerSuccess(): Response
+    {
+        return $this->render('register/success.html.twig');
+    }
 }
